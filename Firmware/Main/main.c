f2cfef979ca9768938def89723f06b997a7e8ba6
LetsPlentendo-CH <letsplentendo@gmail.com>
2020-05-09 15:19:57 +0200

    start support for math operations

--- Firmware/Main/main.c
+++ Firmware/Main/main.c
@@ -17,9 +17,11 @@
 
 int cursorX = 0, cursorY = 0;
 char currLine[0x80] = "";
+int resBuf[0x80];
 int lastButton = -1;
 
 void buttonPressed(int);
+void doCalculation(char*, int*);
 
 int main(void) {
 	disp_initialize();
@@ -34,8 +36,8 @@ int main(void) {
 	
 	disp_clear();
 	
-	for (int i = 0; i < 62; i++) {
-		disp_drawChar((i * 8) % 128, i / 16 * 16, i, FNT_MD);
+	for (int i = 0; i < 70; i++) {
+		disp_drawChar((i * 8) % 128, i / 16 * 16, i, FNT_SM);
 	}
 	
 	_delay_ms(2000);
@@ -57,8 +59,60 @@ int main(void) {
 }
 
 void buttonPressed(int buttonID) {
-	currLine[cursorX] = '0' + buttonID;
-	disp_drawChar(cursorX * 8, 0, currLine[cursorX] - '0', FNT_MD);
-	cursorX++;
+	if (buttonID < 10) {
+		currLine[cursorX] = '0' + buttonID;
+		disp_drawChar(cursorX * 8, 0, currLine[cursorX] - '0', FNT_MD);
+		cursorX++;
+	} else {
+		if (buttonID < 14) {
+			currLine[cursorX] = 62 - 10 + buttonID;
+			disp_drawChar(cursorX * 8, 0, currLine[cursorX], FNT_MD);
+			cursorX++;
+		}
+		if (buttonID == 14) {
+			doCalculation(currLine, resBuf);
+			int i = 0;
+			while (resBuf[i] != 0) {
+				disp_drawChar(i * 8, 16, resBuf[i], FNT_MD);
+				i++;
+			}
+		}
+	}
+}
+
+void doCalculation(char* input, int* output) {
+	int i = 0;
+	int bufNum = 0;
+	int result = 0;
+// 	while (input[i] > 0) {
+// 		int numLength = 0;
+// 		while (input[i] < 10) numLength++, i++;
+// 		for (int j = 0; j < numLength; j++) {
+// 			bufNum += input[j] * pow(10, j);
+// 		}
+// 		switch (input[i]) {
+// 			case 10:
+// 				result += bufNum;
+// 				break;
+// 			case 11:
+// 				result -= bufNum;
+// 				break;
+// 			case 12:
+// 				result /= bufNum;
+// 				break;
+// 			case 13:
+// 				result *= bufNum;
+// 				break;
+// 		}
+// 		i++;
+// 	}
+	int j = 0;
+	while (result >= 0) {
+		output[j] = result % 10;
+		result /= 10;
+		j++;
+		if (result == 0) break;
+	}
+	output[j] = 0;
 }
 
