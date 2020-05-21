91aa957d697c6a56788775335acc2b758a58f0d4
LetsPlentendo-CH <liam.braun@studmail.kzo.ch>
2020-05-22 01:08:56 +0200

    Use new GUI functions in the firmware

--- Firmware/main.c
+++ Firmware/main.c
@@ -3,12 +3,13 @@
 #include "common/display.h"
 #include "common/buttons.h"
 #include "common/eep.h"
+#include "common/gui.h"
 
 #include <math.h>
+#include <stdio.h>
 
 #ifdef console
 	#include <windows.h>
-	#include <stdio.h>
 	#define _delay_ms Sleep
 	DWORD WINAPI ConsoleListener(void* data) {
 		while (1) {
@@ -31,11 +32,11 @@
 
 int cursorX = 0, cursorY = 0;
 char currLine[0x80] = "";
-int resBuf[0x80];
+char resBuf[0x80];
 int lastButton = -1;
 
 void buttonPressed(int);
-void doCalculation(char*, int*);
+void doCalculation(char*, char*);
 
 int main(void) {
 	#ifdef console
@@ -46,20 +47,22 @@ int main(void) {
 	eep_initialize();
 	
 	disp_clear();
-	
-	disp_drawImage();
+
+	gui_drawImage();
 	
 	_delay_ms(2000);
 
 	disp_clear();
 	
-	for (int i = 0; i < 70; i++) {
-		disp_drawChar((i * 8) % 128, i / 16 * 16, i, FNT_SM);
-	}
+	/*for (int i = 0; i < 70; i++) {
+		gui_drawChar((i * 8) % 128, i / 16 * 16, i, FNT_SM, 0);
+	}*/
+	gui_drawString("Test String", 0, 0, FNT_SM, 0);
 	
 	_delay_ms(2000);
 	
 	disp_clear();
+	gui_tabButton("Menu", 4);
 	
 	while (1) {
 		int currButton = buttons_getPressed();
@@ -83,22 +86,18 @@ void buttonPressed(int buttonID) {
 	}
 	if (buttonID < 10) {
 		currLine[cursorX] = buttonID;
-		disp_drawChar(cursorX * 8, 0, currLine[cursorX], FNT_MD);
+		gui_drawChar(cursorX * 8, 0, currLine[cursorX], FNT_MD, 1);
 		cursorX++;
 	} else {
 		if (buttonID < 14) {
 			currLine[cursorX] = 62 - 10 + buttonID;
-			disp_drawChar(cursorX * 8, 0, currLine[cursorX], FNT_MD);
+			gui_drawChar(cursorX * 8, 0, currLine[cursorX], FNT_MD, 1);
 			cursorX++;
 		}
 		if (buttonID == 14) {
 			currLine[cursorX] = -1;
 			doCalculation(currLine, resBuf);
-			int i = 0;
-			while (resBuf[i] != 255) {
-				disp_drawChar(i * 8, 16, resBuf[i], FNT_MD);
-				i++;
-			}
+			gui_drawString(resBuf, 0, 16, FNT_MD, 0);
 		}
 	}
 }
@@ -112,9 +111,9 @@ long extrNum(char *input, int* pos) {
 	return bufNum;
 }
 
-void doCalculation(char* input, int* output) {
+void doCalculation(char* input, char* output) {
 	int i = 0;
-	long result = 0;
+	double result = 0;
 	int isFirstNum = 1;
 	while (input[i] > -1) {
 		if (isFirstNum == 1) {
@@ -140,13 +139,6 @@ void doCalculation(char* input, int* output) {
 			}
 		}
 	}
-	int len = floor(log10(result));
-	int p = 0;
-	do {
-		output[len - p] = result % 10;
-		result /= 10;
-		p++;
-	} while (result > 0);
-	output[p] = 255;
+	sprintf(resBuf, "%f", result);
 }
 
