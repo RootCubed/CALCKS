59969dc1b14922cf6212960b2da17c0418391916
TheCodingBoy <liam.braun@hotmail.ch>
2020-09-02 00:56:32 +0200

    forgot this file

--- Firmware/console/buttons.c
+++ Firmware/console/buttons.c
@@ -1,6 +1,23 @@
 #include "../common/buttons.h"
 #include <stdio.h>
 
+const char BUTTON_MAP[30] = {
+    14, // enter
+    -1, // button doesn't work
+    -1, 0, -1,
+    10, // plus
+    -1, // button doesn't work
+    1, 2, 3,
+    11, // minus
+    -1, // button doesn't work
+    4, 5, 6,
+    12, // div
+    -1, // button doesn't work
+    7, 8, 9,
+    13, // mult
+    -1, -1, -1, -1
+};
+
 char buttonBuffer = -1;
 
 void buttons_initialize() {}
