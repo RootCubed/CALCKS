33c6c6eab2e2bd156b10f1775c28a613c208bf7e
LetsPlentendo-CH <liam.braun@studmail.kzo.ch>
2020-09-02 23:30:25 +0200

    Make buttons.c compatible with new hardware

--- Firmware/avr/buttons.c
+++ Firmware/avr/buttons.c
@@ -6,33 +6,42 @@
 #include "../common/buttons.h"
 #include "../common/gui.h"
 
-const char BUTTON_MAP[30] = {
-    14, // enter
-    -1, // button doesn't work
-    -1, 0, -1,
-    10, // plus
-    -1, // button doesn't work
-    1, 2, 3,
-    11, // minus
-    -1, // button doesn't work
-    4, 5, 6,
-    12, // div
-    -1, // button doesn't work
-    7, 8, 9,
-    13, // mult
-    -1, -1, -1, -1
+const char BUTTON_MAP[25] = {
+    -1, -1, 0, -1, 14,
+	-1, 1, 2, 3, 10,
+	-1, 4, 5, 6, 11,
+	-1, 7, 8, 9, 12,
+	-1, -1, -1, -1, 13
 };
 
 void buttons_initialize() {
 	DDRD = BUTTONS_SR_DATA; // SR pins as output
 	DDRB |= BUTTONS_SR_CLEAR | BUTTONS_SR_CLK; // SR pins as output
-	DDRD &= ~(BUTTONS_ROW_0 | BUTTONS_ROW_1 | BUTTONS_ROW_2 | BUTTONS_ROW_3 | BUTTONS_ROW_4); // rows as input
-	PORTD &= ~(BUTTONS_ROW_0 | BUTTONS_ROW_1 | BUTTONS_ROW_2 | BUTTONS_ROW_3 | BUTTONS_ROW_4); // no pull-up
+	DDRD &= ~(BUTTONS_COL_0 | BUTTONS_COL_1 | BUTTONS_COL_2 | BUTTONS_COL_3 | BUTTONS_COL_4); // cols as input
+	PORTD &= ~(BUTTONS_COL_0 | BUTTONS_COL_1 | BUTTONS_COL_2 | BUTTONS_COL_3 | BUTTONS_COL_4); // no pull-up
+
+	DDRC &= ~(BUTTON_ON_OFF | BUTTON_UP | BUTTON_LEFT | BUTTON_RIGHT | BUTTON_DOWN);
+	PORTC = (BUTTON_ON_OFF | BUTTON_UP | BUTTON_LEFT | BUTTON_RIGHT | BUTTON_DOWN);
+	PORTC = 0xFF;
 
 	PORTB &= ~BUTTONS_SR_CLEAR; // clear SR
 	PORTB |= BUTTONS_SR_CLEAR;
 
 	PORTB |= BUTTONS_SR_CLK; // clock high by default
+
+	DDRB &= ~(_BV(PORTB0));
+	PORTB |= _BV(PORTB0);
+}
+
+char buttons_get_special() {
+	char res = 0;
+	char buf[16];
+	sprintf(buf, "%02x%02x", PINC, DDRC);
+	gui_draw_string(buf, 100, 60, 0, 0);
+	
+	sprintf(buf, "%02x", PINB);
+	gui_draw_string(buf, 100, 40, 0, 0);
+	return res;
 }
 
 int buttons_getPressed() {
@@ -47,19 +56,20 @@ int buttons_getPressed() {
 
 	int res = -1;
 	char buf[16];
-	for (int i = 0; i < BUTTONS_NUM_COLUMNS; i++) {
-		char pressed = PIND & (BUTTONS_ROW_0 | BUTTONS_ROW_1 | BUTTONS_ROW_2 | BUTTONS_ROW_3 | BUTTONS_ROW_4);
-		sprintf(buf, "%02x", pressed);
-		gui_draw_string(buf, 50, 8 * i, 0, 0);
-		if (PIND & pressed) {
-			int row = 0;
-			while (((pressed >> row) & 1) == 0 && row < 5) row++;
-			res = (BUTTONS_NUM_COLUMNS - i - 1) * 5 + row;
-			//break;
+	for (int i = 0; i < BUTTONS_NUM_ROWS; i++) {
+		char pressed = PIND & (BUTTONS_COL_0 | BUTTONS_COL_1 | BUTTONS_COL_2 | BUTTONS_COL_3 | BUTTONS_COL_4);
+		pressed >>= 2; // the column pins start at D2, not D0.
+		/*sprintf(buf, "%02x", pressed);
+		gui_draw_string(buf, 50, 8 * i, 0, 0);*/
+		if (PIND & (pressed << 2)) {
+			int col = 0;
+			while (((pressed >> col) & 1) == 0 && col < 5) col++;
+			res = (BUTTONS_NUM_ROWS - i - 2) * 5 + col;
+			break;
 		}
 		BUTTONS_SR_PULSE();
 	}
 	sprintf(buf, "%02d", res);
-	gui_draw_string(buf, 100, 60, 0, 0);
+	gui_draw_string(buf, 100, 50, 0, 0);
 	return res;
 }
--- Firmware/common/buttons.h
+++ Firmware/common/buttons.h
@@ -1,24 +1,31 @@
 #ifndef BUTTONS_H_
 #define BUTTONS_H_
 
-#define BUTTONS_SR_DATA _BV(PORTD7)
+#define BUTTONS_SR_DATA  _BV(PORTD7)
 #define BUTTONS_SR_CLEAR _BV(PORTB2)
-#define BUTTONS_SR_CLK _BV(PORTB1)
-#define BUTTONS_ROW_0 _BV(PORTD2)
-#define BUTTONS_ROW_1 _BV(PORTD3)
-#define BUTTONS_ROW_2 _BV(PORTD4)
-#define BUTTONS_ROW_3 _BV(PORTD5)
-#define BUTTONS_ROW_4 _BV(PORTD6)
+#define BUTTONS_SR_CLK   _BV(PORTB1)
+#define BUTTONS_COL_0    _BV(PORTD2)
+#define BUTTONS_COL_1    _BV(PORTD3)
+#define BUTTONS_COL_2    _BV(PORTD4)
+#define BUTTONS_COL_3    _BV(PORTD5)
+#define BUTTONS_COL_4    _BV(PORTD6)
 
-#define BUTTONS_NUM_COLUMNS 8
+#define BUTTON_ON_OFF _BV(PORTC2)
+#define BUTTON_UP     _BV(PORTC3)
+#define BUTTON_LEFT   _BV(PORTC4)
+#define BUTTON_RIGHT  _BV(PORTC5)
+#define BUTTON_DOWN   _BV(PORTC6)
+
+#define BUTTONS_NUM_ROWS 8
 
 #define BUTTONS_SR_PULSE() PORTB &= ~BUTTONS_SR_CLK;PORTB |= BUTTONS_SR_CLK
 
 
-extern const char BUTTON_MAP[30];
+extern const char BUTTON_MAP[25];
 
 
 void buttons_initialize();
+char buttons_get_special();
 int buttons_getPressed();
 #ifdef console
     void set_button(char);
